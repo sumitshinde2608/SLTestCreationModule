7a86f9850afa4d91916485df943d3407c8ffdf19
sumitshinde2608 <shindesumit268@gmail.com>
2021-08-12 02:55:58 +0530

    Structure updated

---
+++ src/Containers/SelectTestDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SelectTestDetails() {
+	return (
+		<div>
+			<h1>Select Test Details</h1>
+			<p>Select the test details for the selected test.</p>
+		</div>
+	);
+}
+
+export default SelectTestDetails;
