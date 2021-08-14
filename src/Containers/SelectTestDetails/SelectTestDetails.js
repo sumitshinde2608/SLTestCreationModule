cec148c3cbf0b1166094bea29f713595675276c7
sumitshinde2608 <shindesumit268@gmail.com>
2021-08-15 02:46:01 +0530

    asked form options added

--- src/Containers/SelectTestDetails/SelectTestDetails.js
+++ src/Containers/SelectTestDetails/SelectTestDetails.js
@@ -8,7 +8,7 @@ const SelectTestDetails = () => {
 			<div className=' main '>
 				<br />
 				<Row>
-					<Form.Label column='lg' lg={2}>
+					<Form.Label column lg={2}>
 						Test Name
 					</Form.Label>
 					<Col sm={3}>
@@ -20,7 +20,7 @@ const SelectTestDetails = () => {
 				<br />
 				<Row>
 					<Form.Label column lg={2}>
-						No. of questions
+						Number of questions
 					</Form.Label>
 					<Col sm={3}>
 						<span>
@@ -36,6 +36,20 @@ const SelectTestDetails = () => {
 				<br />
 				<Row>
 					<Form.Label column lg={2}>
+						Number of questions attempts
+					</Form.Label>
+					<Col sm={3}>
+						<span>
+							<Form.Control type='text' placeholder='0' />
+						</span>
+					</Col>
+					<Col>
+						<Form.Text>Cannot be more than the number of questions</Form.Text>
+					</Col>
+				</Row>
+				<br />
+				<Row>
+					<Form.Label column lg={2}>
 						Maximum Marks
 					</Form.Label>
 					<Col sm={3}>
@@ -45,6 +59,34 @@ const SelectTestDetails = () => {
 					</Col>
 					<Col>
 						<Form.Text>Maximum marks should not be more than 1000</Form.Text>
+					</Col>
+				</Row>
+				<br />
+				<Row>
+					<Form.Label column lg={2}>
+						Marking Scheme
+					</Form.Label>
+
+					<Form.Label column lg={2}>
+						Positive Marks
+					</Form.Label>
+
+					<Col sm={1}>
+						<span>
+							<Form.Control type='text' placeholder='0' />
+						</span>
+					</Col>
+					<Form.Label column lg={2}>
+						Negative Marks
+					</Form.Label>
+
+					<Col sm={1}>
+						<span>
+							<Form.Control type='text' placeholder='0' />
+						</span>
+					</Col>
+					<Col>
+						{/* <Form.Text>Maximum marks should not be more than 1000</Form.Text> */}
 					</Col>
 				</Row>
 				<br />
